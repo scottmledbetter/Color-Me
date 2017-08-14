66e9888940f80a561b41dee9af662cc93d32e443
OCSC SAILING <scottmledbetter@gmail.com>
2017-08-14 14:26:48 -0700

    Styled input placeholder

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
+//Update BG Color with field input
+
 $('#color-search').on('input', function () {
 	var colorInput = $('#color-search').val();
 	console.log(colorInput);
     $('body').css('background-color', colorInput); 
 });
+
+//Toggle placeholder with focus//
+
+var placeHolder = 'Type in a color!';
+
+$('#color-search').attr('placeholder', placeHolder);
+
+$('#color-search').focus( function() {
+	$(this).removeAttr('placeholder');
+});
+
+$('#color-search').blur( function() {
+	$(this).attr('placeholder', placeHolder);
+});
+
+//Starting on color value object
 
 var colors = {
     white: 'FFFFFF',
