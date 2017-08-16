7f5fef1542125f43f83931a35a30500afd5ea111
scottmledbetter <scottmledbetter@gmail.com>
2017-08-15 18:09:50 -0700

    Full color list made and formatted to object

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $('#color-search').on('input', function () {
     $('body').css('background-color', colorInput); 
 });
 
-//Toggle placeholder with focus//
+//Toggle placeholder with focus
 
 var placeHolder = 'Type in a color!';
 
@@ -20,24 +20,11 @@ $('#color-search').blur( function() {
 	$(this).attr('placeholder', placeHolder);
 });
 
-//Starting on color value object
-
-var colors = {
-    white: 'FFFFFF',
-    black: '000000',
-    red: 'ff0000',
-    salmon: 'FF8C69'
-};
-
-//console.log(Object.entries(colors)[0]);
-//console.log(Object.keys(colors)[0]);
-//console.log(Object.values(colors)[0]);
-
 //Result display Hex or Name based on input
 
 $(document).ready(function() {
     $('#color-description-container')
-    .append('<i><b>Pssst!</b></i> The HEX value for Salmon is #<span class="textHex">FF8C69</span>');
+    .append('<i><b>Pssst!</b></i> The HEX value for Salmon is <span class="textHex">FF8C69</span>');
 });
 
 $('#color-search').on('input', function () {
@@ -49,8 +36,8 @@ $('#color-search').on('input', function () {
 
 	 for(i = 0; i < objectHexValue.length; i++) {
 
-	 	if(colorInput === objectColorName[i]) {
-			var foundHexValue= objectHexValue[i];
+	 	if(colorInput.toUpperCase() === objectColorName[i].toUpperCase()) {
+			var foundHexValue = objectHexValue[i];
 			$('.textHex').text(foundHexValue);
 		}
 
