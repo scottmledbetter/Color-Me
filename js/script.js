7e832b5ecbafcbfa4b22e6c6aed7a8d6751e7c19
scottmledbetter <scottmledbetter@gmail.com>
2017-08-15 17:03:20 -0700

    HEX value below input matches color name in object

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 //Update BG Color with field input
 
+
 $('#color-search').on('input', function () {
-    var colorInput = $('#color-search').val();
+	var colorInput = $('#color-search').val();
     $('body').css('background-color', colorInput); 
 });
 
@@ -28,13 +29,31 @@ var colors = {
     salmon: 'FF8C69'
 };
 
-console.log(Object.entries(colors)[0]);
-console.log(Object.keys(colors)[0]);
-console.log(Object.values(colors)[0]);
+//console.log(Object.entries(colors)[0]);
+//console.log(Object.keys(colors)[0]);
+//console.log(Object.values(colors)[0]);
 
 //Result display Hex or Name based on input
 
 $(document).ready(function() {
     $('#color-description-container')
-    .append('<i><b>Pssst!</b></i> The HEX value for Salmon is #FF8C69');
+    .append('<i><b>Pssst!</b></i> The HEX value for Salmon is #<span class="textHex">FF8C69</span>');
+});
+
+$('#color-search').on('input', function () {
+
+	 var colorInput = $('#color-search').val();
+	 var objectHexValue = Object.values(colors);
+	 var objectColorName = Object.keys(colors);
+	 var foundHexValue;
+
+	 for(i = 0; i < objectHexValue.length; i++) {
+
+	 	if(colorInput === objectColorName[i]) {
+			var foundHexValue= objectHexValue[i];
+			$('.textHex').text(foundHexValue);
+		}
+
+	 }
+
 });
