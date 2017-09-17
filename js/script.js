1ad22f73822bb8c659344063940b0300afeeee4c
scottmledbetter <scottmledbetter@gmail.com>
2017-09-17 00:32:55 -0700

    Matches data both ways

--- js/script.js
+++ js/script.js
@@ -1,43 +1,52 @@
 //Populate info line
+
 const placeHolder = 'Type in a color!';
 
-$(document).ready(function() {
-    $('#color-description-container')
-    .append('<i><b>Pssst!</b></i> The HEX value for <b><span class="color-name">salmon</span></b> is <b><span class="text-hex">#FF8C69</span><b>');
-	$('#color-search').attr(placeHolder); 
-});
+$('#color-search').attr('placeholder', placeHolder);
 
 //Remove placeholder when input selected
 
-$('#color-search').focus( function() {
-	$(this).removeAttr('placeholder');
+$('#color-search').on('focus', function() {
+  $(this).removeAttr('placeholder');
 });
 
-$('#color-search').blur( function() {
-	$(this).attr('placeholder', placeHolder);
+$('#color-search').on('blur', function() {
+  $(this).attr('placeholder', placeHolder);
 });
 
 //Changes via text input
 
 $('#color-search').on('input', function () {
+  
+  //Reset field
+  
+  $('#color-description-container').html('');
+  $('#color-search').removeClass('bold')
+  $('body').css('background-color', 'gray'); 
 
-	const colorInput = $('#color-search').val();
-	const objectHexValue = Object.values(colors);
-	const objectColorName = Object.keys(colors);
+  const colorInput = $('#color-search').val();
+  const objectHexValue = Object.values(colors);
+  const objectColorName = Object.keys(colors);
+  
+  //Match color name and HEX value of matched color in info line
 
-	//Match color name and HEX value of matched color in info line
-	for(i = 0; i < objectHexValue.length; i++) {
+  for(i = 0; i < objectHexValue.length; i++) {
 
-		if(colorInput.toUpperCase().replace(/ /g,'') === objectColorName[i].toUpperCase().replace(/ /g,'')) {
-			const foundHexValue = objectHexValue[i];
-			$('.text-hex').text(foundHexValue);
-			$('.color-name').text(objectColorName[i].replace( /([a-z])([A-Z])/g, "$1 $2").toLowerCase());
-		}
+    if(colorInput.toUpperCase().replace(/ /g,'') === objectColorName[i].toUpperCase().replace(/ /g,'') 
+      || colorInput.toUpperCase().replace(/#/g,'') === objectHexValue[i].replace(/#/g,'')) {
 
-	}
-    
-	//Update BG Color with field input
-	$('body').css('background-color', colorInput.replace(/ /g,'')); 
+      const foundHexValue = objectHexValue[i];
+      const colorName = '<span class="color-name"></span>';
+      const textHex = '<span class="text-hex"></span>';
 
+      $('#color-description-container').append('The HEX value for ' + colorName + ' is ' + textHex);
+      $('.text-hex').text(foundHexValue);
+      $('.color-name').text(objectColorName[i].replace( /([a-z])([A-Z])/g, "$1 $2").toLowerCase());
+      $('#color-search').addClass('bold');
+      $('body').css('background-color', foundHexValue);
+
+    }
+
+  }
+ 
 });
-
