f3db123c28c9c2f08646c2d3b36458b5f59fef70
scottmledbetter <scottmledbetter@gmail.com>
2018-06-19 16:55:12 -0700

    Converting to vanilla JS. Made code easier to read. Fetches live, updated JSON now

--- app/js/main.js
+++ app/js/main.js
@@ -1,40 +1,45 @@
+// Fetch Data
+const colorData = [];
+fetch('https://raw.githubusercontent.com/meodai/color-names/master/dist/colornames.json')
+  .then(raw => raw.json())
+  .then(data => colorData.push(...data));
+
 //Changes via text input
 
-$('#color-search').on('input', function () {
+const input = document.querySelector('#color-search');
+input.addEventListener('keyup', handleColors);
+
+function handleColors(e) {
 
   //Reset field
-
-  $('#color-description-container').html('');
+  $('#color-description-container').html('')
   $('#color-search').removeClass('bold');
   $('html').css('background-color', 'gray'); 
   $('input').css({'color':'white','border-color':'white'});
 
   //Establish variables
+  const inputValue = $('#color-search').val();
 
-  const input = $('#color-search').val();
-  const objHex = Object.values(colors);
-  const objName = Object.keys(colors);
-  const colorName = '<span class="color-name"></span>';
-  const textHex = '<span class="text-hex"></span>';
-
-  function getContrast50(hexcolor){
-    return (parseInt(hexcolor, 16) > 0xffffff/2) ? 'black':'white';
+  function getContrast50(hex){
+    return (parseInt(hex, 16) > 0xffffff/2) ? 'black':'white';
   }
 
   //Match color name and HEX value of matched color in info line
+  for(let i = 0; i < colorData.length; i++) {
 
-  for(let i = 0; i < objHex.length; i++) {
-
-  if(input.toUpperCase().replace(/ /g,'') === objName[i].toUpperCase().replace(/ /g,'') || input.toUpperCase().replace(/#/g,'') === objHex[i].toUpperCase().replace(/#/g,'')) {
+  if(inputValue.toUpperCase().replace(/ /g,'') === colorData[i].name.toUpperCase().replace(/ /g,'') 
+    || inputValue.toUpperCase().replace(/#/g,'') === colorData[i].hex.toUpperCase().replace(/#/g,'')) {
       
-    $('#color-description-container').append(`The HEX value ${colorName} is ${textHex}`);
-    $('.text-hex').text(objHex[i]);
-    $('.color-name').text(objName[i].replace( /([a-z])([A-Z])/g, "$1 $2").toLowerCase());
+    $('#color-description-container')
+      .append(`The HEX value <span class="bold">${colorData[i].hex}</span> is <span class="bold">${colorData[i].name}</span>`);
+    $('.text-hex').text(colorData[i].hex);
+    $('.color-name').text(colorData[i].name.replace( /([a-z])([A-Z])/g, "$1 $2").toLowerCase());
     $('#color-search').addClass('bold');
-    $('html').css('background-color', objHex[i]);
+    $('html').css('background-color', colorData[i].hex);
 
-    $('input, div').css('color', getContrast50(objHex[i].replace('#','')));
-    $('input, div').css('border-color', getContrast50(objHex[i].replace('#','')));
+    $('input, div').css('color', getContrast50(colorData[i].hex.replace('#','')));
+    $('input, div').css('border-color', getContrast50(colorData[i].hex.replace('#','')));
     } 
+
   }
-});
+}
